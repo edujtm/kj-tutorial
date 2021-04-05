ce8fc4614c7c13228e8dcd8900aea51e8287102a
Eduardo Macedo <eduzemacedo@hotmail.com>
2021-04-04 21:05:24 -0300

    Simplifica as asserções nos testes unitários

--- frontend/src/app/services/todos.service.spec.ts
+++ frontend/src/app/services/todos.service.spec.ts
@@ -34,7 +34,7 @@ describe('TodosService', () => {
   })
 
   // Testar metodo getTodos do todoService
-  it('deve obter todos da API quando chamar getTodos', (done) => {
+  it('deve obter todos da API quando chamar getTodos', () => {
     // Given: a api retorna 2 itens de to-dos
     httpSpy.get.and.returnValue(of({ todos: testTodos }));
 
@@ -43,22 +43,12 @@ describe('TodosService', () => {
 
     // Then: o estado local deve armazenar os todos da API
     expect(todosService.state.todos).toEqual(testTodos);
-    done();
   })
 
-  it('deve obter to-dos da API quando chamar getTodos (HttpClientTestingModule)', (done) => {
+  it('deve obter to-dos da API quando chamar getTodos (HttpClientTestingModule)', () => {
     // System Under Test (SUT)
     const todosService = TestBed.inject(TodosService); 
     const httpMock = TestBed.inject(HttpTestingController);
-
-    todosService.todos$
-      .pipe(
-        expectEvents([
-          [],           // Estado Inicial
-          testTodos     // Resposta da API
-        ], done)
-      )
-      .subscribe()
 
     // When: chama-se o metodo getTodos
     todosService.getTodos();
@@ -66,6 +56,8 @@ describe('TodosService', () => {
     const req = httpMock.expectOne('http://localhost:4000/todos/');
     expect(req.request.method).toEqual('GET');
     req.flush({ todos: testTodos });
+
+    expect(todosService.state.todos).toEqual(testTodos);
   })
 
   // Testar metodo de delete do todo
@@ -75,7 +67,7 @@ describe('TodosService', () => {
   //  todos: Todo[],
   //  todo: Todo
   // }
-  it('deve remover item no estado local após sucesso no DELETE da API de todos', (done) => {
+  it('deve remover item no estado local após sucesso no DELETE da API de todos', () => {
     httpSpy.delete.and.returnValue(of({}))
 
     // Given: já foram obtido os todos da API.
@@ -86,7 +78,6 @@ describe('TodosService', () => {
     todosService.deleteTodo(testTodos[0].id);
 
     expect(todosService.state.todos).toEqual(testTodos.slice(1, testTodos.length));
-    done();
   })
 
   // Testar metodo de update do todo
@@ -96,7 +87,7 @@ describe('TodosService', () => {
   //  todos: Todo[],
   //  todo: Todo
   // }
-  it('deve atualizar item no estado local após sucesso no PUT da API de todos', (done) => {
+  it('deve atualizar item no estado local após sucesso no PUT da API de todos', () => {
     // Given: há um update na API
     httpSpy.put.and.callFake((endpoint, data) => {
       return of({ todo: { ...testTodos[1], ...data } })
@@ -110,16 +101,9 @@ describe('TodosService', () => {
     const updatedTodo = { ...testTodos[1], description: novaDescricao };
     const updatedTodos = [ testTodos[0], updatedTodo];
 
-    todosService.todos$
-      .pipe(
-        expectEvents([
-          testTodos,
-          updatedTodos     // Resposta da API
-        ], done)
-      )
-      .subscribe()
-
     todosService.updateTodo(2, updatedTodo);
+
+    expect(todosService.state.todos).toEqual(updatedTodos);
   })
 });
 
